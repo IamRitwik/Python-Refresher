8daa4706895e2d78912286865afbadb2f5248963
IamRitwik <iamritwik01@gmail.com>
2025-02-10 21:00:11 +0530

    load excel into pandas data frame

---
+++ 5.excel-to-pdf/main.py
@@ -0,0 +1,10 @@
+from fpdf import FPDF
+import pandas as pd
+import glob
+
+filepaths = glob.glob("Invoice/*.xlsx")
+print(filepaths)
+
+for filepath in filepaths:
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    print(df)
