b7a8ef76aa5fc874931d45e744d8eee8facee091
IamRitwik <iamritwik01@gmail.com>
2025-02-11 19:28:06 +0530

    email daily news API

--- 6.daily-news-API/main.py
+++ 6.daily-news-API/main.py
@@ -1,13 +1,20 @@
 import requests
+from sendEmail import sendEmail
 
-api_key = "58fddba2e9a24b36bc899db833a0446b"
-
-url = "https://newsapi.org/v2/"
+url = "https://newsapi.org/v2/everything?q=tesla&from=2025-01-11&sortBy=publishedAt&apiKey=58fddba2e9a24b36bc899db833a0446b"
 
 request = requests.get(url)
 content = request.json()
 
 articles = content["articles"]
 
+body = ""
 for article in articles:
-    print(article["title"])
+    title = article["title"]
+    description = article["description"]
+    if description is not None and title is not None:
+        body = body + title + "\n" + description + "\n" + "\n"
+
+body = body.encode("utf-8")
+print(body)
+#sendEmail(body)
